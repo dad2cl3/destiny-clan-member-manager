8f2b1debd95d7d47480b0210b95ab1a09e0e8d0b
Jason Chalmers <jachal@Jasons-MacBook-Pro.local>
2017-02-07 13:44:43 -0500

    Added create table scripts for clan member, character, and Slack account data

---
+++ sql/stg/create_table-t_clan_members.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS stg.t_clan_members;
+
+CREATE TABLE stg.t_clan_members (
+	effective_date date NOT NULL,
+	clan_id int4 NOT NULL,
+	bungie_id int4,
+	bungie_name varchar(100),
+	destiny_id int8 NOT NULL,
+	destiny_name varchar(100) NOT NULL,
+	approval_date date NOT NULL
+);
---
+++ sql/stg/create_table-t_slack_accounts.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS stg.t_slack_accounts;
+
+CREATE TABLE stg.t_slack_accounts (
+	effective_date date NOT NULL,
+	slack_id varchar(25) NOT NULL,
+	user_name varchar(50) NOT NULL
+);
+
+COMMENT ON TABLE stg.t_slack_accounts IS 'Raw staging of Slack accounts';
